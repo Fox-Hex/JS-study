c303e4b46b9f575cd9f5c4cd79b8c319fe205852
Fox-Hex <maksbright56@gmail.com>
2022-09-11 23:32:29 +0300

    array study

--- arrays/array.js
+++ arrays/array.js
@@ -1,6 +1,65 @@
-// definition
-const items = []
-const numbers = [one, two, three]
+// // definition
+// const items = []
+// const numbers = [1,2,3,4,5]
 
-console.log(numbers.at(1));
-console.log('dsad');
+// // using .at() for array index, can use -1 for last element
+// console.log(numbers.at(0));
+
+
+
+// //1st task
+// const getWeekends = (parametr = 'long') => {
+//     const longNames = ['saturday', 'sunday']
+//     const shortNames = ['sat', 'sun']
+//     if (parametr === 'long') {
+//       return longNames
+//     } else if (parametr === 'short') {
+//       return shortNames
+//     } else {
+//       return
+//     }
+// }
+// console.log(getWeekends());
+
+
+
+// // array changing
+
+// const animals = ['cat', 'dog', 'mouse', 'frog']
+// console.log(animals);
+
+// animals.push('horse')
+// console.log(animals);
+
+// animals.unshift('bird')
+// console.log(animals);
+
+// animals[7] = 'fish'
+// console.log(animals);
+
+// delete animals[3]
+// console.log(animals);
+
+
+
+
+
+// // 2nd task
+
+// const swap = (incArray) => {
+//     if (incArray.length <= 1) {
+//       return incArray
+//     } else {
+//         const lastElem = incArray.at(-1)
+//         incArray[incArray.length-1] = incArray[0]
+//         incArray[0] = lastElem
+//         return incArray
+//     }
+// }
+
+// const a = [1,2,3,4,5]
+// console.log(swap(a));
+
+
+
+
