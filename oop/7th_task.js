c80f84b4d05723c5ba3204a965ee019c3245c355
Fox-Hex <maksbright56@gmail.com>
2022-11-11 01:21:01 +0300

    h 11.11

--- oop/7th_task.js
+++ oop/7th_task.js
@@ -1,13 +1,13 @@
-function magic(...arguments) {
-  let sum = 0;
-  arguments.forEach((elem) => sum = sum + elem)
-  return (...arguments2) => magic.valueOf
+function magic(...arg) {
+  const sum = arg.reduce((accum, elem) => accum + elem, 0)
+  const inner = (...arg2) => magic(sum, ...arg2)
+  inner.valueOf = () => sum
+  return inner
 }
 
-magic.valueOf = () => 0
-magic()
+
+console.log(+magic(5,6));
 console.log(magic() == 0); // true
 console.log(magic(5, 2, -8) == -1); // true
 console.log(magic(1, 2)(3, 4, 5)(6)(7, 10) == 38); // true
 console.log(magic(4, 8, 1, -1, -8)(3)(-3)(7, 2) == 13); // true
-
