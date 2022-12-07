f4de92d1b95683f9cfa28726339db7b72b60b446
Fox-Hex <maksbright56@gmail.com>
2022-12-07 17:12:00 +0300

    w 7.12

--- ood/4th_task.js
+++ ood/4th_task.js
@@ -1,23 +1,27 @@
 const normalize = (countries) => {
-  const norm = countries.map((elem) => {
+  
+  const normArray = countries.map((elem) => {
     const name = elem.name.toLowerCase().trim()
     const country = elem.country.toLowerCase().trim()
     return {name, country}
   })
-  console.table(norm);
 
-  const set = new Set(norm.map(elem => elem.country))
-  const val = Array.from(set)
-  const val2 = Object.fromEntries(val.map(elem => [elem, []]))
+  const uniqCountries = [... new Set(normArray.map(elem => elem.country))]
 
-  const a = {b:[]}
-  console.log(a.b.push(1));
-  return val2
+  const result = uniqCountries.reduce((acc, cur) => {
+    acc[cur] = normArray
+      .filter(elem => elem.country === cur)
+      .map(elem => elem.name)
+      .sort()
+    return acc
+  }, {})
+  
+  return result
 }
-
 const countries = [
   { name: 'Miami', country: 'usa' },
   { name: 'samarA', country: '  ruSsiA' },
+  { name: 'Moscow ', country: ' Russia' },
   { name: 'Moscow ', country: ' Russia' },
 ];
 console.log(normalize(countries));
