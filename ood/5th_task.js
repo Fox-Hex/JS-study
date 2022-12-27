a5ab7fbed1edc278e3cfb15f15bea1a3e00022f9
Fox-Hex <maksbright56@gmail.com>
2022-12-27 17:04:25 +0300

    w 27.12

--- ood/5th_task.js
+++ ood/5th_task.js
@@ -2,11 +2,21 @@ import * as yup from 'yup';
 
 const getInvalidBooks = (books) => {
   const shema = yup.object().shape({
+    name: yup.string().required(),
+    author: yup.string().required(),
+    pagesCount: yup.number().positive().integer(),
+    link: yup.string().url(),
+    genre: yup.string().oneOf(genres)
   })
 
-  return
+  return books.map(elem => shema.validateSync(elem))
 }
 
+// name – строка, обязательное
+// author – строка, обязательное
+// pagesCount – положительное число, необязательное
+// link – строка url, необязательное, не может быть пустой строкой; ссылка на книгу в интернете
+// genre – строка, необязательное; жанр книги. Должен входить в список определенный в файле index.js
 
 const genres = [
   'drama', 'horror', 'fantasy', 'classic',
@@ -18,3 +28,4 @@ const books = [
 ];
 
 const invalidBooks = getInvalidBooks(books); // [{ author: 'author 2' }]
+console.log(invalidBooks);
