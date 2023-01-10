d3ea758ff5c05ce61da184d69966ff63742607d0
Fox-Hex <maksbright56@gmail.com>
2023-01-10 03:54:44 +0300

    h 10.01

--- ood/6th_task.js
+++ ood/6th_task.js
@@ -1,16 +1,21 @@
 const protect = (obj) => {
 
   const handlers = {
+
     get: (target, prop) => {
       if (prop.startsWith('_')) {
         throw 'Error'
       }
       if (prop in target) {
+        if (typeof target[prop] === 'function') {
+          return target[prop].bind(target)
+        }
         return target[prop]
       } else {
         throw 'Error'
       }
     },
+    
     set: (target, prop, value, receiver) => {
       if (prop.startsWith('_')) {
         throw 'Error'
@@ -19,7 +24,7 @@ const protect = (obj) => {
       return true
     }
   }
-  
+
   return new Proxy(obj, handlers)
 }
 
@@ -44,5 +49,5 @@ console.log(course._name); // "Object-oriented design"
 console.log(course._nonExists); // undefined
  
 console.log(protectedCourse._name); // Error
-// protectedCourse._name = 'OOD'; // Error
-// protectedCourse._nonExists; // Error
+protectedCourse._name = 'OOD'; // Error
+console.log(protectedCourse._nonExists); // Error
