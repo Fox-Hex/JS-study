fb36549d874e0a6219a4be142d83c6f353bc9e73
Fox-Hex <maksbright56@gmail.com>
2023-03-27 00:09:54 +0300

    27

--- front-architecture/index.js
+++ front-architecture/index.js
@@ -1,17 +1,54 @@
+// const app = () => {
+//   const result = document.getElementById('result')
+//   const button = document.querySelector('#increment')
+
+//   button.addEventListener('click', () => {
+//     result.textContent = parseInt(result.textContent, 10) +1
+//   })
+// }
+
+// const articleCount = () => {
+//   const articles = document.querySelectorAll('.article')
+//   return articles.length
+// }
+
+
+// app()
+// document.querySelector('#articleCount').innerHTML = `count of articles: ${articleCount()}`
+
+
 const app = () => {
-  const result = document.getElementById('result')
-  const button = document.querySelector('#increment')
-
-  button.addEventListener('click', () => {
-    result.textContent = parseInt(result.textContent, 10) +1
-  })
+  const state = {
+    registrationForm: {
+      valid: null,
+      errors: [],
+    },
+  };
+  
+  const input = document.querySelector(".phone");
+  const submit = document.querySelector(".submit");
+  
+  input.addEventListener("keyup", (e) => {
+    const inputValue = e.target.value;
+  
+    state.registrationForm.value = inputValue;
+  
+    if (inputValue.match(/^\d+$/)) {
+      state.registrationForm.valid = true;
+      state.registrationForm.errors = [];
+    } else {
+      state.registrationForm.valid = false;
+      state.registrationForm.errors.push("wrong format");
+    }
+  
+    submit.disabled = !state.registrationForm.valid;
+  
+    if (state.registrationForm.valid) {
+      input.style.border = null;
+    } else {
+      input.style.border = "thick solid red";
+    }
+  });  
 }
-
-const articleCount = () => {
-  const articles = document.querySelectorAll('.article')
-  return articles.length
-}
-
 
 app()
-document.querySelector('#articleCount').innerHTML = `count of articles: ${articleCount()}`
