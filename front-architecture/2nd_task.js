10a1a8f01d6b56fd2ddce8b3c922ad66b9695046
Fox-Hex <maksbright56@gmail.com>
2023-03-29 00:30:19 +0300

    29

--- front-architecture/2nd_task.js
+++ front-architecture/2nd_task.js
@@ -1,21 +1,30 @@
-const routes = {
-  tasksPath: () => '/api/tasks',
-};
+// const routes = {
+//   tasksPath: () => 'file',
+// };
 
-const form = document.querySelector('form')
-const field = document.querySelector('input')
-const button = document.querySelector('button')
-const list = document.querySelector('.list-group')
+// const form = document.querySelector('form')
+// const field = document.querySelector('input')
+// const button = document.querySelector('button')
+// const list = document.querySelector('.list-group')
 
-field.focus()
+// field.focus()
 
-button.addEventListener('click', async (e) => {
-  e.preventDefault()
+// button.addEventListener('click', async (e) => {
+//   e.preventDefault()
 
-  const li = document.createElement('li')
-  li.append(field.value)
+//   const li = document.createElement('li')
+//   li.append(field.value)
   
-  const response = await axios.post(routes.tasksPath(), data)
+//   const data = 'fsdf'
+//   const response = await axios.post(routes.tasksPath(), data)
 
-  form.reset()
-})
+//   form.reset()
+// })
+
+const b = async () => {
+
+  const a = await axios.get('files/tasks')
+  console.log(a.data);
+}
+
+b()
