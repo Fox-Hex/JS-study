aec8de3a2173225e5e663eb74cdef94e841ab391
Fox-Hex <maksbright56@gmail.com>
2023-04-29 17:30:44 +0300

    29.04

--- advanced-testing/__tests__/2nd_task.test.js
+++ advanced-testing/__tests__/2nd_task.test.js
@@ -7,15 +7,18 @@ const __filepath = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filepath)
 const dirPath = `${__dirname}/../__fixtures__`
 
-const list = await fs.readdir(dirPath)
+const list = (await fs.readdir(dirPath)).filter(elem => elem !== 'result.html')
 const result = await fs.readFile(`${dirPath}/result.html`, 'utf-8')
 
+const functions = [right1, wrong1, wrong2, wrong3]
+const testName = ['right1', 'wrong1', 'wrong2', 'wrong3']
 
-
-test.each(list)('right1', async (elem) => {
-  if (elem !== 'result.html') {
-    const elemFilePath = path.join(dirPath, elem)
-    const html = await right1(elemFilePath)
-    expect(html).toBe(result.trim())
-  }
+functions.forEach((funcElem, i) => {
+  test.each(list)(`${testName[i]} %s`, async (listElem) => {
+      const elemFilePath = path.join(dirPath, listElem)
+      const html = await funcElem(elemFilePath)
+      expect(html).toBe(result.trim())
+  })
 })
+
+
