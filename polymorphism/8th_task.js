b509128b8780566896d43bf83696dc52453f3caa
Fox-Hex <maksbright56@gmail.com>
2023-05-08 00:35:28 +0300

    8

--- polymorphism/8th_task.js
+++ polymorphism/8th_task.js
@@ -1,6 +1,28 @@
 import axios from 'axios'
-import path from 'path'
 
-const path1 = 'https://ide-js-polymorphism-dependency-inversion-5469787.evaluator7-5.hexlet.io/'
-const path2 = '/api/v2/cities/'
+// const apiUrl = 'http://localhost:8080/api/v2/'; // ???????????????
 
+class WeatherService {
+  constructor(city, request) {
+    this.url = apiUrl + 'cities/' + city
+    this.request = request
+  }
+  async getWeather() {
+    const weatherRequest = await this.request.get(this.url)
+    const weatherObj = JSON.parse(weatherRequest)
+    return `Temperature in ${weatherObj.name}: ${weatherObj.temperature}C`
+  }
+}
+
+class Request {
+  constructor() {
+  }
+  async get(url) {
+    const response = await axios(url)
+    return response.data
+  }
+}
+
+const city = process.argv[2]
+const weather = new WeatherService(city, new Request())
+console.log(await weather.getWeather())
