84dffb604aa90743d75d34cadd218138d7bef57b
Fox-Hex <maksbright56@gmail.com>
2023-05-15 23:48:42 +0300

    re 15

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Card from './tasks/1st_task.jsx'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('container'));
 root.render(<Card />);
