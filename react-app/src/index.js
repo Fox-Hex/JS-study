e9b8fa418f7724d486a5b832abe9effcf0526f68
Fox-Hex <maksbright56@gmail.com>
2023-05-17 02:11:36 +0300

    17.05

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -41,15 +41,84 @@ import ReactDOM from 'react-dom/client';
 
 
 
-// task 3
-//
+// // task 3
+// //
+// import Card from './tasks/3rd_task.jsx'
+// const root = ReactDOM.createRoot(document.getElementById('container'));
 
-import Card from './tasks/3rd_task.jsx'
-const root = ReactDOM.createRoot(document.getElementById('container'));
+// // const title = "title 11111";
+// // const text = "text 11111";
+// // root.render(<Card title={title} text={text} />);
 
-// const title = "title 11111";
-// const text = "text 11111";
-// root.render(<Card title={title} text={text} />);
+// // без пропсов
+// root.render(<Card />);
 
-// без пропсов
-root.render(<Card />);
+
+
+// const arra = ['task1', 'task2', 'task3', 'task4', 'task5', 'task6']
+// class List extends React.Component {
+//   render() {
+//     let key = 1
+//     return (
+//       <ul>
+//         {arra.map(elem => <li key={key+=1}>{elem}</li>)}
+//         <li>Элемент без key</li>
+//         <li>Ещё один элемент без key</li>
+//       </ul>
+//     )
+//   }
+// }
+// const root = ReactDOM.createRoot(document.getElementById('container'));
+// root.render(<List />);
+
+// class Article extends React.Component {
+//   render() {
+//     const { header, body } = this.props;
+
+//     return (
+//       <article>
+//         <h1>Late news:</h1>
+//         <Section header={header} body={body}/>
+//       </article>
+//     );
+//   }
+// }
+// class Section extends React.Component {
+//   render() {
+//     const { header, body } = this.props;
+
+//     return (
+//       <>
+//         <h2>{header}</h2>
+//         <div>{body}</div>
+//       </>
+//     );
+//   }
+// }
+// const root = ReactDOM.createRoot(document.getElementById('container'));
+// root.render(<Article header='Important' body='Something very important just happened...' />);
+
+// class Component extends React.Component {
+//   render() {
+//     return [
+//       <div key='one'>one</div>,
+//       <div key='two'>two</div>
+//     ];
+//   }
+// }
+// const root = ReactDOM.createRoot(document.getElementById('container'));
+// root.render(<Component />);
+
+
+
+// // task 4
+// //
+// import DefinitionsList from './tasks/4th_task.jsx'
+
+// const definitions = [
+//   { dt: 'one', dd: 'two', id: 1 },
+//   { dt: 'another term', dd: 'another description', id: 2 },
+// ];
+
+// const root = ReactDOM.createRoot(document.getElementById('container'));
+// root.render(<DefinitionsList data={definitions} />);
---
+++ react-app/src/tasks/4th_task.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+class DefinitionsList extends React.Component {
+  render() {
+    const { data } = this.props
+    if (data.length === 0) {
+      return null
+    }
+
+    const tags = data.map(({ dt, dd, id }) => (
+      <React.Fragment key={id}>
+        <dt>{dt}</dt>
+        <dd>{dd}</dd>
+      </React.Fragment>
+    ))
+
+    return <dl>{tags}</dl>
+  }
+}
+
+export default DefinitionsList
