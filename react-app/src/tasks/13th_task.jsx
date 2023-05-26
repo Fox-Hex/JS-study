03f7c0e51b1ba06bc449750d4e9f60f430d665ab
Fox-Hex <maksbright56@gmail.com>
2023-05-26 15:10:55 +0300

    er

--- react-app/src/tasks/13th_task.jsx
+++ react-app/src/tasks/13th_task.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { uniqueId } from 'lodash';
 
 class Item extends React.Component {
   render() {
@@ -8,9 +9,9 @@ class Item extends React.Component {
       <div>
         <div className="row">
           <div className="col-auto">
-            <button type="button" className="btn btn-primary btn-sm" onClick={remove(id)}>-</button>
+            <button type="button" className="btn btn-primary btn-sm" onClick={remove(listElement.id)}>-</button>
           </div>
-          <div className="col">{listElement}</div>
+          <div className="col">{listElement.text}</div>
         </div>
         <hr />
       </div>
@@ -28,14 +29,16 @@ class TodoBox extends React.Component {
     this.setState({ currentText: e.target.value })
   }
 
-  formSubmit = (e) => {
+  formSubmit = (elemId) => (e) => {
     e.preventDefault()
-    const newTasks = [this.state.currentText, ...this.state.tasks]
-    this.setState({ currentText: '', tasks: newTasks })
+    const text = this.state.currentText
+    const newTask = [{text: text, id: elemId}, ...this.state.tasks]
+    this.setState({ currentText: '', tasks: newTask })
+    console.log(this.state);
   }
 
   remove = (taskId) => () => {
-    const newTasks = this.state.tasks.filter(elem => (elem, id) => id !== taskId)
+    const newTasks = this.state.tasks.filter(elem => elem.id !== taskId)
     this.setState({tasks: newTasks})
   }
 
@@ -44,7 +47,7 @@ class TodoBox extends React.Component {
     return (
       <div>
         <div className="mb-3">
-          <form className="d-flex" onSubmit={this.formSubmit}>
+          <form className="d-flex" onSubmit={this.formSubmit(uniqueId())}>
             <div className="me-3">
               <input type="text" value={currentText} required="" className="form-control"
                 placeholder="I am going..." onChange={this.changeText} />
@@ -71,7 +74,7 @@ class TodoBox extends React.Component {
           </form>
         </div>
 
-        {tasks.map((elem, id) => {
+        {tasks.map((elem) => {
           return (
             <Item task = {elem} onRemove = {this.remove}/>
           )
